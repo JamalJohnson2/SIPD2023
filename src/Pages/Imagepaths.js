2f95cf93b1d97c9b9274f35ee51ddcfae39a6d01
JamalJohnson2 <150681547+JamalJohnson2@users.noreply.github.com>
2023-11-23 23:29:23 +0800

    Add files via upload

---
+++ src/Pages/Imagepaths.js
@@ -0,0 +1,6 @@
+const imagePaths = {
+    Headerimg: require('../Assets/Images/Header-img.png'),
+    Contentimg: require('../Assets/Images/Fb-ui-img.png'),
+};
+  
+export default imagePaths;
---
+++ src/Pages/Navigation/Nav.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import '../Navigation/Nav.css';
+import { Nav } from '../Animation';
+
+const Navbar = () => {
+  return (
+    <Nav/>
+  );
+};
+  
+  export default Navbar;
